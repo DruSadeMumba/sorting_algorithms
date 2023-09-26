0b9ca7b2784cd10a8a646af594f9d0e504543350
Dru Mumba <drusademumba@gmail.com>
2023-09-26 15:52:06 +0300

    Write a function that sorts an array of integers in ascending order using the Radix sort algorithm

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -54,12 +54,11 @@ void count_sorting(int *array, size_t size, int exp)
 */
 void radix_sort(int *array, size_t size)
 {
-	int exp, max;
+	int exp, max = get_max(array, size);
 
 	if (!array || size < 2)
 		return;
 
-	max = get_max(array, size);
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
 		count_sorting(array, size, exp);
