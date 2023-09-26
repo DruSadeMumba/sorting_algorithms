84ed7fc182552bcd5aa08ca6a88d5f4a704c299b
Dru Mumba <drusademumba@gmail.com>
2023-09-26 15:33:40 +0300

    fix err

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -23,17 +23,11 @@ int get_max(int *array, size_t size)
  * @size: size
  * @exp: digit
 */
-void count_sorting(int *array, size_t size, int exp)
+void count_sorting(int *array, size_t size, int exp, int *output)
 {
-	int *output = malloc(sizeof(int) * size);
-	int count[10] = {0};
+	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	size_t i;
 
-	if (!output)
-	{
-		fprintf(stderr, "Malloc fail\n");
-		exit(EXIT_FAILURE);
-	}
 	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
 	for (i = 0; i < 10; i++)
@@ -41,11 +35,10 @@ void count_sorting(int *array, size_t size, int exp)
 	for (i = size - 1; (int)i >= 0; i--)
 	{
 		output[count[(array[i] / exp) % 10] - 1] = array[i];
-		count[(array[i] / exp) % 10] -= 1;
+		count[(array[i] / exp) % 10]--;
 	}
 	for (i = 0; i < size; i++)
 		array[i] = output[i];
-	free(output);
 }
 
 /**
@@ -56,13 +49,21 @@ void count_sorting(int *array, size_t size, int exp)
 void radix_sort(int *array, size_t size)
 {
 	int exp = 1, max = get_max(array, size);
+	int *output = malloc(sizeof(int) * size);
+
+	if (!output)
+	{
+		fprintf(stderr, "Malloc fail\n");
+		exit(EXIT_FAILURE);
+	}
 
 	if (!array || size < 2)
 		return;
 
 	for (; max / exp > 0; exp *= 10)
 	{
-		count_sorting(array, size, exp);
+		count_sorting(array, size, exp, output);
 		print_array(array, size);
 	}
+	free(output);
 }
